638959bf8b93ddd6776efa1dc10e841dcff527db
Yura <yurii.o@ukr.net>
2018-01-04 17:31:46 +0200

    Initial Commit

--- js/js.js
+++ js/js.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
    $('a[href^="#"]').click(function () { 
      elementClick = $(this).attr("href");
      destination = $(elementClick).offset().top;
-       $('html').animate( { scrollTop: destination }, 1100 );
+       $('html').animate( { scrollTop: (destination - 175) }, 1100 );
      return false;
    });
  });
